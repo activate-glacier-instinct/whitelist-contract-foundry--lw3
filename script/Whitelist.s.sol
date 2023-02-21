9a3ce0be2ebc4882f663b37b6896d71b99b2a2e8
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-21 01:04:09 +0000

    FIX : adds remappings and fixes script

--- script/Whitelist.s.sol
+++ script/Whitelist.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
+import "forge-std/src/Script.sol";
 import { Whitelist } from '../src/Whitelist.sol';
 
 contract WhitelistScript is Script {
